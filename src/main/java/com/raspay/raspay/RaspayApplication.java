fcf4df4f552556ff338538b6fbb38ad945a744f0
Joel Esteves <joel.esteves@hotmail.com>
2024-01-03 21:54:18 -0300

    initial commit

---
+++ src/main/java/com/raspay/raspay/RaspayApplication.java
@@ -0,0 +1,13 @@
+package com.raspay.raspay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raspay/raspay/RaspayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raspay.raspay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaspayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
